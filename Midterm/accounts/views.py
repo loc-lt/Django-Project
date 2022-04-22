c15501ebdc6d005f0ba96981d1e871d07df26eca
loclethanhm10hcmus <lochcmus@gmail.com>
2022-04-22 19:43:28 +0700

    dynamic url routing and templates

--- Midterm/accounts/views.py
+++ Midterm/accounts/views.py
@@ -1,5 +1,6 @@
 from curses.ascii import HT
 from math import remainder
+from multiprocessing import context
 import re
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -28,5 +29,10 @@ def products(request):
 
     return render(request, 'accounts/products.html', {'products': products})
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk_test):
+    customer = Customer.objects.get(id=pk_test)
+    orders = customer.order_set.all()
+    order_count = orders.count()
+
+    context = {'customer': customer, 'orders': orders, 'order_count': order_count}
+    return render(request, 'accounts/customer.html', context)
