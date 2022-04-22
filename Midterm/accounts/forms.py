361438da3aa24f67deb1a1a87b14aec6dab79404
loclethanhm10hcmus <lochcmus@gmail.com>
2022-04-22 23:06:47 +0700

    create update and delete with model forms

---
+++ Midterm/accounts/forms.py
@@ -0,0 +1,10 @@
+from dataclasses import field
+from django.forms import ModelForm
+from .models import Order
+
+class OrderForm(ModelForm):
+    class Meta:
+        model = Order
+        fields = '__all__'
+         
+        
